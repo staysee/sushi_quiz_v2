f2d82bec74bca996d1fc064b151050dfe551688e
Stacey Sugiono <staceysugiono@gmail.com>
2018-06-05 15:21:20 -0700

    added questions to store

---
+++ questions.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const STORE = [
+	{
+		question: "What is known as Japanese horseradish?",
+		answers: [
+				"wasabi".
+				"nigori",
+				"mooli",
+				"ninjin"
+		],
+		correctAnswer: "wasabi",
+	},
+	{
+		question: "Which of these is the name for geoduck?",
+		answers: [
+				"hokkigai",
+				"mirugai",
+				"akagai",
+				"tairagai"
+		],
+		correctAnswer: "mirugai"
+	},
+	{
+		question: "What does \"sushi\" actually mean?",
+		answers: [
+				"raw fish",
+				"sashimi",
+				"rice with vinegar",
+				"seaweed and rice"
+		],
+		correctAnswer: "rice with vinegar"
+	{
+		question: "Which is not an egg?",
+		answers: [
+				"ikura",
+				"tamago",
+				"tobiko",
+				"awabi"
+		],
+		correctAnswer: "awabi",
+	},
+	{
+		question: "Besides water, what is the main ingredient in soy sauce?",
+		answers: [
+				"water",
+				"wheat",
+				"salt",
+				"soybeans"
+		],
+		correctAnswer: "wheat"
+	},
+	{
+		question: "Sushi is most closely associated with which Japanese city?",
+		answers: [
+				"Kyoto",
+				"Tokyo",
+				"Osaka",
+				"Sapporo"
+		],
+		correctAnswer: "Tokyo"
+	},
+	{
+		question: "Nori is: ",
+		answers: [
+				"green algae",
+				"blue algae",
+				"red algae",
+				"brown algae"
+		],
+		correctAnswer: "red algae"
+	},
+	{
+		question: "What part of the sea urchin (uni) is eaten?",
+		answers: [
+				"brain",
+				"eggs",
+				"gonads",
+				"tongue"
+		],
+		correctAnswer: "gonads"
+	},
+	{
+		question: "Which of these is not tuna?"
+		answers: [
+				"Skipjack",
+				"Albacore",
+				"Bluefin",
+				"Hamachi"
+		],
+		correctAnswer: "Hamachi"
+	},
+	{
+		question: "Who is often considered the \"world\'s greatest\" sushi chef?"
+		answers: [
+				"Daisuke Nakazawa",
+				"Nobu Matsuhisa",
+				"Jiro Ono",
+				"Masaharu Morimoto"
+		],
+		correctAnswer: "Jiro Ono"
+	}
+]
