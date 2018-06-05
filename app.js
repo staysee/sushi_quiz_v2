881912940394b8394cc1b199fd3765111f136ec9
Stacey Sugiono <staceysugiono@gmail.com>
2018-06-05 15:58:26 -0700

    writing function stubs

---
+++ app.js
@@ -0,0 +1,71 @@
+
+'use strict'
+
+let questionNumber = 0;
+let correctScore = 0;
+let incorrectScore = 0;
+
+//game starts
+function startGame(){
+
+}
+
+//display question
+function displayQuestion(){
+
+}
+//display answer choices
+function displayAnswerChoices(){
+
+}
+
+function displayScore(){
+
+}
+
+
+//check for correct answer
+function checkCorrectAnswer(){
+
+}
+//show current question and scores
+function updateScore(){
+
+}
+
+//textual feedback for wrong answer
+function answerIsWrong(){
+
+}
+
+//textual feedback for right answer
+function answerIsRight(){
+
+}
+
+
+// EVENT HANDLERS
+
+//user click play button
+function handleStart(){
+
+}
+
+//new game (play again)
+function newGame(){
+
+}
+
+//user click to submit answerchoice
+function handleSubmit(){
+
+}
+
+//user click for next question
+function handleNext(){
+//update question number
+}
+
+function createQuiz(){
+	//all the functions to run on load
+}
