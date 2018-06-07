6076a0e9c9dd5330fafc2e766eae89cb764adaa8
Stacey Sugiono <staceysugiono@gmail.com>
2018-06-07 13:55:08 -0700

    working on answerChoices

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-'use strict'
-
 let questionNumber = 0;
 let correctScore = 0;
 let incorrectScore = 0;
@@ -8,22 +6,41 @@ let incorrectScore = 0;
 //game starts
 function startGame(){
 	$('#start-container').addClass("hidden");
-  $('#quiz-container').removeClass("hidden");
-  render();
+	$('#quiz-container').removeClass("hidden");
+	render();
 }
 
 //display question
 function displayQuestion(){
   const questionNumber = $('.question-number');
   const questionArea = $('.question-area');
-  questionArea.html(`${STORE[0].question}`);
+  questionArea.html(`${STORE[questionNumber].question}`);
 
-  console.log(`Current question is: ${STORE[0].question}`);
-
+  console.log(`Current question is: ${STORE[questionNumber].question}`);
+  console.log('`displayQuestion` runnning.')
 }
-//display answer choices
+
 function displayAnswerChoices(){
-  const
+	$('#quiz-form').append(generateAnswerChoices());
+	console.log('`displayAnswerChoices` running')
+}
+
+//display answer choices
+function generateAnswerChoices(){
+	return `
+		 <label class="answerChoice">
+            <input type="radio" name="option" value="" required>${STORE[questionNumber].answers[0]}
+          </label>
+          <label class="answerChoice">
+            <input type="radio" name="option" value="" required>${STORE[questionNumber].answers[1]}
+          </label>
+          <label class="answerChoice">
+            <input type="radio" name="option" value="" required>${STORE[questionNumber].answers[2]}
+          </label>
+          <label class="answerChoice">
+            <input type="radio" name="option" value="" required>${STORE[questionNumber].answers[3]}
+          </label>
+	`
 }
 
 function updateScore(){
@@ -49,9 +66,7 @@ function answerIsRight(){
 
 }
 
-function render(){
 
-}
 
 // EVENT HANDLERS
 
@@ -75,7 +90,10 @@ function handleNext(){
 //update question number
 }
 
-
+function render(){
+	displayQuestion();
+	displayAnswerChoices();
+}
 
 function createQuiz(){
 	//all the functions to run on load
